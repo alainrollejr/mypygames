16651d8e3cea381abb6a184de0e8ceca2eb6e966
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-03-03 15:46:24 +0100

    herschikken

--- wesley1/wesley1.py
+++ wesley1/wesley1.py
@@ -12,56 +12,54 @@ import random
 
 pygame.init()
 
-display_width = 800
-display_height = 600
+white = (255,255,255)
+black = (0,0,0)
+red = (200,0,0)
+light_red = (255,0,0)
+yellow = (200,200,0)
+light_yellow = (255,255,0)
+green = (34,177,76)
+light_green = (0,255,0)
+smallfont = pygame.font.SysFont("comicsansms", 25)
+medfont = pygame.font.SysFont("comicsansms", 50)
+largefont = pygame.font.SysFont("comicsansms", 85)
+clock = pygame.time.Clock()
+
+''' vanaf hier wordt het interessant
+'''
+display_width = 800 # breedte
+display_height = 600  # hoogte
+beeldjes_per_seconde = 40
 
 scherm = pygame.display.set_mode((display_width,display_height))
 
 
-pygame.display.set_caption('boze Wesley')
-
-#icon = pygame.image.load("apple.png")       
-#pygame.display.set_icon(icon)
-
-white = (255,255,255)
-black = (0,0,0)
-
-
-red = (200,0,0)
-light_red = (255,0,0)
-
-yellow = (200,200,0)
-light_yellow = (255,255,0)
-
-green = (34,177,76)
-light_green = (0,255,0)
-
-clock = pygame.time.Clock()
+pygame.display.set_caption('boze Wesley game')
 
 
 
-
+''' posities
+'''
 wesley_x = 0
 wesley_y = 0
 
 
-
-
-
-smallfont = pygame.font.SysFont("comicsansms", 25)
-medfont = pygame.font.SysFont("comicsansms", 50)
-largefont = pygame.font.SysFont("comicsansms", 85)
-
 wesley = pygame.image.load('images/wesley_w283_h290_px.png')
-#appleimg = pygame.image.load('apple.png')
+wesley_b = wesley.get_width() # breedte in pixels
+wesley_h = wesley.get_height() # hoogte in pixels
 
-crashed = False
+print(wesley_b)
+print(wesley_h)
 
-while not crashed:
+
+''' game loop
+'''
+stop = False
+while not stop:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            crashed = True
+            stop = True
         print(event)
         
     scherm.fill(green)
@@ -70,8 +68,11 @@ while not crashed:
     
     wesley_x = wesley_x + 1
     print('wesley_x=' + str(wesley_x))
+    
+    ''' scherm vernieuwen
+    '''
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(beeldjes_per_seconde)
     
 pygame.quit()
 quit()
