0619828bedfee86e5d70240e36779fa62d05694c
alainrollejr <35453310+alainrollejr@users.noreply.github.com>
2018-03-04 09:55:46 +0100

    adding while example

---
+++ python_examples/while_example.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  4 08:20:00 2018
+
+@author: aro
+
+
+voorbeeld van "while"
+
+"""
+
+teller = 0
+
+'''
+    "while" = zolang als
+'''
+
+while teller < 10:
+    teller = teller + 1
+    print(teller)
+
+
+    
+
