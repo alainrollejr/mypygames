317ca08e8b40c161e0ea2c55ee99341984fa5209
alain rolle <aro@newtec.eu>
2019-04-05 18:18:50 +0200

    backup

--- reinforcement_learning/blackjack.py
+++ reinforcement_learning/blackjack.py
@@ -30,12 +30,9 @@ pi = [] # policy
 Q = [] # action value function
 
 # episode variables
-dealer_cards = []
-player_cards = []
 
-def init_episode():  
-    dealer_cards = []
-    player_cards = []
+
+def init_episode(dealer_cards, player_cards):  
     
     dealer_cards.append(random.choice(POSSIBLE_CARDS)) #this is what the dealer shows
     dealer_cards.append(random.choice(POSSIBLE_CARDS))
@@ -66,24 +63,41 @@ def main(argv):
     parser = argparse.ArgumentParser(description='MC e-greedy policy iteration on Sutton and Barto Blackjack problem example')
     
     
-    parser.add_argument('-m','--method', help='v:  value iteration, p: policy iteration', required=False)
+    parser.add_argument('-m','--method', help='e:  epson greedy, s: start', required=False)
+    parser.add_argument('-e','--episodes', help='-e <nr of episodes>', required=False)
     args = vars(parser.parse_args())
     
     
     method = args['method']
+    episodes = args['episodes']
+    
+    if episodes is None:
+        nr_episodes = 1
+    else:
+        nr_episodes = int(episodes)
+    
+        
     
     if method is None:
-        value_iter = True
+        epson_greedy = True
     else:
-        if method=='v':
-            value_iter = True
-        elif method == 'p':
-            value_iter = False
+        if method=='e':
+            epson_greedy = True
+        elif method == 's':
+            epson_greedy  = False
         else:
-            print('unsupported method ',method)
-            value_iter = True
-            
-            
+            print('unsupported method, reverting to default method ',method)
+            epson_greedy = True
+    
+    for k in range(nr_episodes):
+        dealer_cards = []
+        player_cards = []       
+        init_episode(dealer_cards, player_cards)
+    
+
+        print('dealer cards',dealer_cards)
+        print('player cards',player_cards)
+        print('player has usable ace', has_usable_ace(player_cards))         
     
     
 if __name__ == "__main__":
